c8e4f6fe3bcc386a35e7d90db41560ebf394bf51
Enes İlgezdi <enesilgezdi@gmail.com>
2024-10-06 22:43:24 +0300

    Todolari listeye ekleme , silme ve güncelleme işlemlerini yaptım.

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoIosRemoveCircleOutline } from "react-icons/io";
 import { CiEdit } from "react-icons/ci";
 import { FaCheck } from "react-icons/fa";
 import '../css/Todo.css'
+import { TodoType } from '../types/Types';
+import { useDispatch } from 'react-redux';
+import { removeTodoById, updateTodo } from '../redux/todoSlice';
 
-function Todo() {
+interface TodoProps {
+    todoProps: TodoType
+}
+
+function Todo({ todoProps }: TodoProps) {
+
+    const { id, content } = todoProps;
+
+    const [editable, setEditAble] = useState<boolean>(false);
+    const [newTodo, setNewTodo] = useState<string>(content);
+
+    const dispatch = useDispatch();
+    const handleRemoveTodo = () => {
+        dispatch(removeTodoById(id));
+    }
+
+    const handleUpdateTodo = () => {
+        const payload: TodoType = {
+            id: id,
+            content: newTodo
+        }
+        dispatch(updateTodo(payload));
+        setEditAble(false);
+    }
+
+
     return (
         <div className='todos'>
-            <div>BEN İLK TODOYUM</div>
+            {editable ?
+                <input
+                    className='todo-input2'
+                    type='text'
+                    value={newTodo}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewTodo(e.target.value)} /> :
+                <div>{content}</div>
+            }
             <div>
-                <IoIosRemoveCircleOutline className='todo-remove' />
-                <CiEdit className='todo-edit' />
+                <IoIosRemoveCircleOutline onClick={handleRemoveTodo} className='todo-remove' />
+                {editable ? <FaCheck onClick={handleUpdateTodo} className='todo-edit' /> : <CiEdit onClick={() => setEditAble(true)} className='todo-edit' />}
+
             </div>
         </div>
     )
